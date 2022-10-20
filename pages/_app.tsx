8085fba4eabce26ccdf2d708722e31bdac672fb8
roy2554 <mail.sewon@gmail.com>
2022-10-21 03:04:06 +0900

    add metadata in page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,18 @@ import type { AppProps } from 'next/app';
 // FontAwesome Configs
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import Head from 'next/head';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <div className="bg-dark-bg">
+            <Head>
+                <title>roy2554</title>
+                <meta name="title" content="roy2554" />
+                <meta name="description" content="roy2554's profile page" />
+            </Head>
+
             <meta name="keywords" content="roy2554, roy2554.me, devroy" />
             <meta name="author" content="roy2554" />
 
