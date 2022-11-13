008f5547df2042aa832da8ccf76818c5c7e8e1b8
roy2554 <mail.sewon@gmail.com>
2022-11-13 22:39:37 +0900

    make cookie work on IOS

--- Utils/tokenManager.ts
+++ Utils/tokenManager.ts
@@ -1,9 +1,24 @@
 import axios from 'axios';
 // import cookie from 'react-cookies';
+import cookie from 'react-cookies';
 function setToken(jwt: string) {
     console.log('setToken', jwt);
     // axios.defaults.headers.common.Authorization = 'Bearer ' + jwt;
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + jwt;
+
+    if (jwt) {
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + jwt;
+
+        // const expires = new Date();
+        // expires.setDate(Date.now() + 60 * 60 * 24 * 7);
+
+        /*
+            Cookie on IOS
+            1. you must set path to '/'
+            2. you must set maxAge(or the cookie will be deleted when the browser is closed. {is set as Session Cookie})
+        */
+        cookie.save('jsonwebtoken', jwt, { path: '/', httpOnly: false, maxAge: 60 * 60 * 24 * 7 });
+    }
+
     // console.log('ax', axios.defaults.headers.common['Authorization']);
 
     // cookie.save('jsonwebtoken', jwt, )
