5e6448d0bab484e4dea36a33c854517d8e3187cd
roy2554 <mail.sewon@gmail.com>
2022-12-31 02:16:13 +0900

    roy2554.me version 2 beta

--- Utils/tokenManager.ts
+++ Utils/tokenManager.ts
@@ -5,7 +5,7 @@ import cookie from 'react-cookies';
 import useStore from '../store';
 
 function setToken(jwt: string) {
-    console.log('setToken', jwt);
+    // console.log('setToken', jwt);
     // axios.defaults.headers.common.Authorization = 'Bearer ' + jwt;
 
     if (jwt) {
@@ -19,12 +19,12 @@ function setToken(jwt: string) {
             1. you must set path to '/'
             2. you must set maxAge(or the cookie will be deleted when the browser is closed. {is set as Session Cookie})
         */
-        cookie.save('jsonwebtoken', jwt, {
-            path: '/',
-            httpOnly: false,
-            maxAge: 60 * 60 * 24 * 7,
-            domain: process.env.NODE_ENV === 'production' ? process.env.COOKIE_TARGET_DOMAIN : undefined,
-        });
+        // cookie.save('jsonwebtoken', jwt, {
+        //     path: '/',
+        //     httpOnly: false,
+        //     maxAge: 60 * 60 * 24 * 7,
+        //     domain: process.env.NODE_ENV === 'production' ? process.env.COOKIE_TARGET_DOMAIN : undefined,
+        // });
     }
 
     // console.log('ax', axios.defaults.headers.common['Authorization']);
